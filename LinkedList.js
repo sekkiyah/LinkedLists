92771edb4ee0f3f66b14a72fbb4aa2856e2bab12
sekkiyah <seth_hinshaw@hotmail.com>
2022-10-22 13:36:36 -0500

    built out data structure functionality

--- LinkedList.js
+++ LinkedList.js
@@ -5,23 +5,56 @@ class LinkedList {
   }
 
   insertAtHead(data) {
-
+    const node = new Node(data);
+    node.next = this.head;
+    this.head = node;
+    this.length++;
   }
 
   getByIndex(idx) {
- 
+    if (idx < 0) {
+      return null;
+    }
+    let currentNode = this.head;
+    let count = 0;
+    while (count < idx) {
+      if (currentNode.next) {
+        currentNode = currentNode.next;
+        count++;
+      } else {
+        return null;
+      }
+    }
+    return currentNode;
   }
 
   insertAtIndex(idx, val) {
-
+    if (idx > this.length || idx < 0) {
+      return null;
+    } else if (idx === 0) {
+      this.insertAtHead(val);
+    } else {
+      const node = new Node(val);
+      node.next = this.getByIndex(idx);
+      this.getByIndex(idx - 1).next = node;
+      this.length++;
+    }
   }
 
   removeHead() {
-
+    this.head = this.head.next;
+    this.length--;
   }
 
   removeAtIndex(idx) {
-
+    if (idx > this.length || idx < 0) {
+      return null;
+    } else if (idx === 0) {
+      this.removeHead();
+    } else {
+      this.getByIndex(idx - 1).next = this.getByIndex(idx).next;
+      this.length--;
+    }
   }
 }
 
